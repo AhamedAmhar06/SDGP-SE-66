9e851599491d90fc027df723ce860ab461097130
AhamedAmhar06 <ahamed0606.jr@gmail.com>
2024-02-20 08:09:24 +0530

    updating question bank algorithm

--- server/qb.js
+++ server/qb.js
@@ -1,32 +1,54 @@
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 // Define the questions
 let questions = [
-    { question: "What is your name?", response: "" },
-    { question: "What is your age?", response: "" },
-    { question: "What is your favorite programming language?", response: "" },
-  ];
-  
-  // Function to ask questions
-  function askQuestion(questionObject) {
-    let response = prompt(questionObject.question);
+  { question: "username ", response: "" },
+  { question: "questions from which topic? ", response: "" },
+  { question: "Q1: Define OOP ", response: "" },
+];
+
+// Function to ask questions
+function askQuestion(questionObject, callback) {
+  rl.question(questionObject.question, (response) => {
     questionObject.response = response;
+    callback();
+  });
+}
+
+// Function to start the questionnaire
+function startQuestionnaire(index = 0) {
+  if (index === questions.length) {
+    rl.close();
+    //displayResponses();
+    saveResponses(); // save the responses 
+    return;
   }
-  
-  // Function to start the questionnaire
-  function startQuestionnaire() {
-    for (let i = 0; i < questions.length; i++) {
-      askQuestion(questions[i]);
-    }
+
+  askQuestion(questions[index], () => startQuestionnaire(index + 1));
+}
+
+// Function to display the responses
+function displayResponses() {
+  for (let i = 0; i < questions.length; i++) {
+    console.log(`${questions[i].question} ${questions[i].response}`);
   }
-  
-  // Function to display the responses
-  function displayResponses() {
-    for (let i = 0; i < questions.length; i++) {
-      console.log(`${questions[i].question} ${questions[i].response}`);
-    }
-  }
-  
-  // Start the questionnaire
-  startQuestionnaire();
-  
-  // Display the responses
-  displayResponses();
+}
+
+// Function to save responses to a file
+function saveResponses() {
+  fs.writeFile('responses.json', JSON.stringify(questions, null, 2), (err) => {
+    if (err) throw err;
+    console.log('Responses saved to responses.json');
+    
+  });
+}
+
+
+// Start the questionnaire
+startQuestionnaire();
