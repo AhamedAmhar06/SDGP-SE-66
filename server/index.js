22157f426ee00e0d22d08df56dcd8bbd5676a7bc
Ahamed Amhar <72806996+AhamedAmhar06@users.noreply.github.com>
2024-03-29 13:52:48 +0530

    Origin updated

--- server/index.js
+++ server/index.js
@@ -92,7 +92,7 @@ io.on("connection", (socket) => {
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://www.undergraduplift.tech/', credentials: true }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
