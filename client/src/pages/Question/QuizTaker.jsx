403d4d4cea8ebc03b93c92647f8a474c7e0ef29c
Ahamed Amhar <72806996+AhamedAmhar06@users.noreply.github.com>
2024-04-01 21:34:40 +0530

    bug

--- client/src/pages/Question/QuizTaker.jsx
+++ client/src/pages/Question/QuizTaker.jsx
@@ -69,7 +69,7 @@ const shuffle = (array) => {
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={resetQuiz}>
             Try Again
           </button>
-          <Link to="/optionSelector" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ">
+          <Link to="/questionBank" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ">
             Go to Question Bank
           </Link>
         </div>
