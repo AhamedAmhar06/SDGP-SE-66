4c696c6bdab49c13bf627949186727c1154f9c5d
Sudarshana Weerakkodi <sudarshana422@gmail.com>
2024-04-05 11:54:46 +0530

    bug fix

--- client/src/pages/Tutoring/TutorDashboard.jsx
+++ client/src/pages/Tutoring/TutorDashboard.jsx
@@ -1,70 +1,189 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Link, useParams } from 'react-router-dom'
-import TutorName from '../../components/ViewRequests/TutorName'
 
+import { useContext, useState, useEffect } from "react";
+import { UndergradContext } from "../../context/undergradContext";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import profileImage from "../../Assets/images/Mask group.png";
+import { GiTeacher } from "react-icons/gi";
+import payments from "../../Assets/images/analysis 1.png";
+import PastSessionTut from "../../components/Dashboard/PastSessionTut";
+import SessionRequestedCardTut from "../../components/Dashboard/SessionRequestCardTut";
+import Notifications from "../../components/Dashboard/Notifications";
+import { MdNotifications } from "react-icons/md";
 
-export default function RequestedSessions({ tutor_id}) {
-  
-  const { id } = useParams();
+export default function TutorDashboard() {
+  const { undergrad } = useContext(UndergradContext);
+  const [ undergradLoaded, setUndergradLoaded ] = useState(false);
+  const [ email, setEmail ] = useState('');
+  const [ tutor, setTutor ] = useState([]);;
   const [ requests, setRequests ] = useState([]);
-  const [ requestsLoaded, setRequestsLoaded ] = useState(false);
 
-  //Load requests
   useEffect(() => {
-    const fetchRequests = async () => {
+    const fetchUndergrad = async () => {
+        try {
+            if(undergrad){
+              const email = undergrad.email;
+              const response = await axios.post('/tutorDetailsByEmail', { email });
+              setTutor(response.data);
+              // const requests = await axios.post('/fetchRequests', { email });
+              // setRequests(requests.data);
+              setUndergradLoaded(true);
+            }
+            
+        } catch (error) {
+            console.error(error);
+        }
+    };
+        fetchUndergrad();
+  });
 
-      try {
-        const response = await axios.post('/getRequests', { id });
-        // Sort requests based on the timestamp
-        const sortedRequests = response.data.sort((a, b) => {
-          return new Date(b.createdAt) - new Date(a.createdAt);
-        });
-        // console.log(sortedRequests);
-        setRequests(sortedRequests);
-
-      } catch (error) {
-        console.log(error);
-      }
-
-    }
-    fetchRequests();
-  })
+  // useEffect(() => {
+  //   const fetchRequests = async () => {
+  //       try {
+  //         if(undergrad){
+  //           const email = undergrad.email;
+  //           const requests = await axios.post('/fetchRequests', { email });
+  //           setRequests(requests.data);
+  //           // console.log(response.data);
+  //         }
+  //       } catch (error) {
+  //           console.error(error);
+  //       }
+  //   };
+  //   fetchRequests();
+  // });
 
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center">View Requests</h1>
-      
-      {/* Display Requests related to the undergrad */}
+      {undergradLoaded ? (
+        <div className="flex flex-col items-center justify-start w-full pt-[70px] bg-gray-50">
+          <div className="flex flex-row justify-center w-full max-w-[1269px]">
+            <div className="flex flex-col w-full gap-[43px] md:flex-row">
+              <div className="flex flex-col items-center justify-start w-full md:w-[30%] gap-[41px]">
+                {/* Profile Section */}
+                <div className="flex flex-col items-center justify-start p-[45px] border-NavBlue border border-solid bg-gray-100 rounded-[36px]">
+                  <GiTeacher   className="h-[100px] w-[100px] ml-2.5 rounded-[20%] hover:scale-110 duration-300 mb-4"  />
+                  {!!undergrad && <h2 className="text-xl font-bold text-center text-NavBlue">Hi {tutor.fName}!</h2>}
+                  <p className="mt-4 font-bold text-center text-s text-NavBlue">Tutor</p>
+                  <p className="mt-2 font-bold text-center text-s text-NavBlue">{tutor.university}</p>
+                  <p className="mt-2 font-bold text-center text-s text-NavBlue">{tutor.bio}</p>
+                  <div className="flex flex-col items-center justify-start w-full mt-[72px]">
+                    <div className="w-full sm:w-[100%] md:w-[100%] lg:w-[100%]">
+                      <Link to="/createCourse">
+                      <button className="w-full px-5 py-3 mt-0 mb-2 duration-300 bg-white border rounded-xl hover:scale-110">Create Course</button>
+                      </Link>
+                      <Link to='/editProfile'>
+                        <button className="w-full px-5 py-3 mt-0 mb-2 duration-300 bg-white border rounded-xl hover:scale-110">Edit Profile</button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
 
-      <ul>
-        {requests.map(request => (
-          <li key={request._id}
-            className={`border m-2 rounded-md ${(request.accepted && !request.decline) ? 'bg-green-200' : 'bg-slate-200'}`}
-          >
-            <div className={`p-2 ${request.decline ? 'bg-red-200' : null}`}>
-              < TutorName tutor_ID={request.tutorID}/>
-              <p>Subject: {request.subject}</p>
-              <p>{request.topic}</p>
-              <p>Reason: {request.reason}</p>
-              <p>Date: {request.date}</p>
-              <p>Time: {request.startTime} - {request.endTime}</p>
-              <p>Status : {request.accepted ? 'Accepted' : null } {request.decline ? 'Declined' : null} {!(request.decline || request.accepted) ? 'Processing' : null }</p>
-              {(request.accepted && !request.completed)? (
-                <a href={`/session/${request._id}`} target="_blank" rel="noopener noreferrer">
-                  <button
-                    className="bg-NavBlue rounded-xl text-white p-2 hover:scale-105 duration-300 m-2"
-                  >
-                    Join Now
-                  </button>
-                </a>
+                {/* Question Bank */}
+                <div className="flex flex-col items-center justify-start w-full p-[31px] border-NavBlue border border-solid rounded-[31px]">
+                  
+                  <div className="flex flex-row justify-start items-center w-[99%] mt-[5px] gap-6">
+                  <div className="flex flex-col items-center justify-start w-[91%] gap-[19px]">
+                  <div className="flex flex-col items-start justify-start w-[97%]">
+                    <h1 className="w-[69%]">
+                      <span className="text-NavBlue font-helveticalight text-[38px] font-normal">
+                        Question
+                        <br />
+                      </span>
+                      <span className="text-NavBlue uppercase text-[32px] font-bold">bank</span>
+                      <span className="text-NavBlue"></span>
+                    </h1>
+                    <div className="flex flex-row justify-start mt-[18px] gap-[5px]">
+                      <button className="font-helvetica font-bold min-w-[55px] bg-gray-100 rounded-[6px] text-sm hover:scale-110 px-[5px] py-[3px] ">  Figma</button>
+                      <button className="font-helvetica font-bold min-w-[53px] bg-gray-100 rounded-[6px] text-sm hover:scale-110 px-[5px] py-[3px] ">   CSS</button>
+                      <button className="font-helvetica font-bold min-w-[131px] bg-gray-100 rounded-[6px] text-sm hover:scale-110  px-[5px] py-[3px]">   machine learning</button>
+                    </div>
+                      <div className="flex flex-row justify-start mt-[5px] gap-1.5">
+                      <button className="font-helvetica font-bold min-w-[60px] bg-gray-100 rounded-[6px] text-sm hover:scale-110 px-[5px] py-[3px] ">  Python</button>
+                      <button className="font-helvetica font-bold min-w-[82px] bg-gray-100 rounded-[6px] text-sm hover:scale-110 px-[5px] py-[3px] ">   JavaScript{" "}</button>
+                      <button className="font-bold border border-black border-solid rounded-md hover:scale-110 px-[5px] py-[1px]">+</button>
+                      </div>
+                    
+                    </div>
+                    <button className="uppercase min-w-[252px] rounded-[18px] bg-gray-400 text-sm hover:scale-110 px-[5px] py-[3px] ">  try a new quizz{" "}</button>
+
+
+                    
+                    
+                    </div>
+                  
+                    </div></div>
+
+              </div>
+              {/* Scheduled Sessions Section */}
+              <div className="flex flex-col items-center justify-start w-full gap-[30px] md:w-[36%]">
+                {/* Session Card Example */}
+                {/* You can map through your scheduled sessions here */}
+                <div className="flex flex-col items-center justify-start w-[99%] p-[31px] bg-NavBlue rounded-[34px]">
+                  {/* Session Heading */}
+                  <div className="flex flex-row justify-between items-start w-[93%]">
+                    <div className="h-[70px] w-[69%] relative">
+                    <Link to={"/notifications"}>
+                      <h3 className="text-3xl font-bold justify-center w-full h-full left-0 bottom-0 right-0 top-0 m-auto !text-white !leading-[35px] absolute transition-transform transform hover:scale-105">
+                        Notifications
+                      </h3>
+                      </Link>
+                    </div>
+                    <MdNotifications className="mt-1 text-6xl text-white ml-14" />
+                  </div>
                 
-              ) : ("Completed")}
+                  <div className="grid w-full grid-cols-1 gap-y-4">
+                    <Notifications />
+
+                  </div>
+
+                </div>
+                <div className="flex flex-col items-center justify-start w-full gap-[34px] md:w-[100%]">
+                      <div className="flex flex-col items-center justify-start w-full p-[29px] border-NavBlue border border-solid rounded-[34px]">
+                        <div className="flex flex-col items-start justify-start w-[96%] mt-1 gap-2.5">
+                        <h1 className="w-[58%] !leading-[39px]">
+                            <span className="text-NavBlue font-helveticalight text-[23px] font-normal">Past session</span>
+                          <span className="text-xl font-normal text-NavBlue font-helveticalight">
+                            <br />
+                          </span>
+                          <span className="text-3xl font-bold uppercase text-NavBlue">records </span>
+                        </h1>
+
+                        <div className="flex flex-row justify-start items-center w-full gap-[29px]">
+                          <div className="flex flex-col w-[92%] mt-[3px] gap-3.5 ">
+                            
+                          <PastSessionTut undergradID={tutor._id}/>
+
+                        </div>
+
+                          <div className="flex flex-col items-center justify-start w-[2%]">
+                         
+                          </div>
+                        </div>            
+                      </div>              
+                      </div>
+                </div>
+              </div>
+              {/* Past Sessions & Reviews Section */}
+              <div className="flex flex-col items-center justify-start w-full gap-[34px] md:w-[28%]">
+                <div className="flex flex-col items-center justify-start w-full p-[31px] border-NavBlue border border-solid rounded-[34px]">
+                    <div className="flex flex-row justify-start items-center w-[97%] mb-[11px] gap-[21px]">
+                      <div className="flex flex-col items-center justify-start w-[92%]">
+                        <Link className="w-[97%] !leading-[35px] transition-transform transform hover:scale-105" to={`/viewRequestsTut/${tutor._id}`}>
+                          <span className="text-NavBlue text-[26px] font-normal">View </span> <br />
+                          <span className="text-4xl font-bold text-NavBlue">Session Requests</span>
+                          <span className="font-normal text-NavBlue"></span>
+                        </Link>
+
+                          <SessionRequestedCardTut undergradID={tutor._id}/>
+                                
+                        </div></div>
+                        </div>
+              </div>
             </div>
-          </li>
-        ))}
-      </ul>
+          </div>
+        </div>
+      ) : (null)}
     </div>
-  )
+  );
 }
